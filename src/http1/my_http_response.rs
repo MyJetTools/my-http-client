a2901f17900a8ee6d4f1ee8051db200a2ba7989e
amigin <amigin@gmail.com>
2024-11-08 10:26:43 +0200

    Small Improvements

--- src/http1/my_http_response.rs
+++ src/http1/my_http_response.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use http::{HeaderMap, StatusCode};
+
 use crate::MyHttpClientDisconnect;
 
 pub enum MyHttpResponse<
@@ -11,4 +13,36 @@ pub enum MyHttpResponse<
         response: crate::HyperResponse,
         disconnection: Arc<dyn MyHttpClientDisconnect + Send + Sync + 'static>,
     },
+}
+
+impl<TStream: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + Sync + 'static>
+    MyHttpResponse<TStream>
+{
+    pub fn status(&self) -> StatusCode {
+        match self {
+            MyHttpResponse::Response(response) => response.status(),
+            MyHttpResponse::WebSocketUpgrade { response, .. } => response.status(),
+        }
+    }
+
+    pub fn headers(&self) -> &HeaderMap {
+        match self {
+            MyHttpResponse::Response(response) => response.headers(),
+            MyHttpResponse::WebSocketUpgrade { response, .. } => response.headers(),
+        }
+    }
+
+    pub fn headers_mut(&mut self) -> &HeaderMap {
+        match self {
+            MyHttpResponse::Response(response) => response.headers_mut(),
+            MyHttpResponse::WebSocketUpgrade { response, .. } => response.headers_mut(),
+        }
+    }
+
+    pub fn into_response(self) -> crate::HyperResponse {
+        match self {
+            MyHttpResponse::Response(response) => response,
+            MyHttpResponse::WebSocketUpgrade { response, .. } => response,
+        }
+    }
 }
