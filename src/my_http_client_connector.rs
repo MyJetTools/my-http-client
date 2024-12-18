16afdf1614dba364c6c2138ae50c63a7d89bb09a
amigin <amigin@gmail.com>
2024-12-18 17:29:25 +0200

    Fixes

--- src/my_http_client_connector.rs
+++ src/my_http_client_connector.rs
@@ -6,7 +6,7 @@ use crate::MyHttpClientError;
 #[async_trait::async_trait]
 pub trait MyHttpClientConnector<TStream: tokio::io::AsyncRead + tokio::io::AsyncWrite> {
     async fn connect(&self) -> Result<TStream, MyHttpClientError>;
-    fn get_remote_endpoint(&self) -> &RemoteEndpoint;
+    fn get_remote_endpoint(&self) -> RemoteEndpoint;
     fn is_debug(&self) -> bool;
 
     fn reunite(read: ReadHalf<TStream>, write: WriteHalf<TStream>) -> TStream;
