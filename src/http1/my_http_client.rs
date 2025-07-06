41a210ac7972b9b50bc2f5fb104adf62e9a6007e
amigin <amigin@gmail.com>
2025-07-06 14:49:30 +0300

    Fixes

--- src/http1/my_http_client.rs
+++ src/http1/my_http_client.rs
@@ -281,3 +281,13 @@ impl<
         });
     }
 }
+
+impl<
+        TStream: tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send + Sync + 'static,
+        TConnector: MyHttpClientConnector<TStream> + Send + Sync + 'static,
+    > From<TConnector> for MyHttpClient<TStream, TConnector>
+{
+    fn from(value: TConnector) -> Self {
+        Self::new(value)
+    }
+}
